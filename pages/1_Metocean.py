e688d98a5fde1d97175349f7d1550bde192eefeb
tanundsen <104429703+tanundsen@users.noreply.github.com>
2026-03-09 17:23:50 +0100

    Update 1_Metocean.py

--- pages/1_Metocean.py
+++ pages/1_Metocean.py
@@ -19,6 +19,10 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_DIR, "..", "metocean_monthclim.nc")
+
 # -----------------------------------------------------------
 # Page setup
 # -----------------------------------------------------------
@@ -164,7 +168,8 @@ clip_pct = 99.6
 # -----------------------------------------------------------
 # Load dataset
 # -----------------------------------------------------------
-ds = load_metocean(nc_path)
+ds = load_metocean(DATA_PATH)
+
 for k in ["prob","hs_edges","tp_edges","lat3_edges","lon3_edges"]:
     if k not in ds:
         st.error(f"Dataset missing {k}")
