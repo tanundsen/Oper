abef2cbcfceff29c1c1deeaeb82937d01dc2418c
tanundsen <104429703+tanundsen@users.noreply.github.com>
2026-03-09 18:15:29 +0100

    Update 1_Metocean.py

--- pages/1_Metocean.py
+++ pages/1_Metocean.py
@@ -132,7 +132,7 @@ with st.sidebar:
     months = ["Jan","Feb","Mar","Apr","May","Jun",
               "Jul","Aug","Sep","Oct","Nov","Dec"]
     month_vals = np.arange(1,13)
-    chosen_label = st.selectbox("Month", months, index=12)
+    chosen_label = st.selectbox("Month", months, index=11)
     label_to_idx = dict(zip(months, month_vals))
 
     st.subheader("Statistic")
